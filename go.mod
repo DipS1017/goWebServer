f13c57d51647e15f6cbf419ba0b0660ea953eb6e
dips <dipeshshrestha910@gmail.com>
2025-01-20 15:54:45 +0545

    bare bone web server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DipS1017/goWebServer
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+  mux:=http.NewServeMux()
+  mux.HandleFunc("/",handleRoot)
+  fmt.Println("Server is running on port 8080")
+  http.ListenAndServe(":8080",mux) 
+
+}
+func handleRoot(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w,"Hello world")
+
+}
